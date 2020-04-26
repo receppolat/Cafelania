ccb4c4e67f78220bb575f00c22e90e75871efd83
Recep Polat <receppolat95@gmail.com>
2020-04-26 16:33:53 +0300

    26.04.2020
    Last Version

--- CafeRestorantOtomasyonu/CafeRestorantOtomasyonu/pdf.Designer.cs
+++ CafeRestorantOtomasyonu/CafeRestorantOtomasyonu/pdf.Designer.cs
@@ -39,11 +39,11 @@
             // axAcroPDF1
             // 
             this.axAcroPDF1.Enabled = true;
-            this.axAcroPDF1.Location = new System.Drawing.Point(-32, 80);
-            this.axAcroPDF1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.axAcroPDF1.Location = new System.Drawing.Point(-103, 98);
+            this.axAcroPDF1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.axAcroPDF1.Name = "axAcroPDF1";
             this.axAcroPDF1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axAcroPDF1.OcxState")));
-            this.axAcroPDF1.Size = new System.Drawing.Size(596, 604);
+            this.axAcroPDF1.Size = new System.Drawing.Size(654, 456);
             this.axAcroPDF1.TabIndex = 0;
             // 
             // label1
@@ -52,25 +52,28 @@
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(30, 27);
+            this.label1.Location = new System.Drawing.Point(40, 33);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(122, 20);
+            this.label1.Size = new System.Drawing.Size(156, 25);
             this.label1.TabIndex = 1;
             this.label1.Text = "Alıcı Mail Adresi:";
             // 
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.textBox1.Location = new System.Drawing.Point(158, 20);
+            this.textBox1.Location = new System.Drawing.Point(211, 25);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(185, 26);
+            this.textBox1.Size = new System.Drawing.Size(245, 30);
             this.textBox1.TabIndex = 2;
             // 
             // btnGonder
             // 
-            this.btnGonder.Location = new System.Drawing.Point(338, 21);
+            this.btnGonder.Location = new System.Drawing.Point(451, 26);
+            this.btnGonder.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnGonder.Name = "btnGonder";
-            this.btnGonder.Size = new System.Drawing.Size(75, 26);
+            this.btnGonder.Size = new System.Drawing.Size(100, 32);
             this.btnGonder.TabIndex = 3;
             this.btnGonder.Text = "Gönder";
             this.btnGonder.UseVisualStyleBackColor = true;
@@ -78,15 +81,15 @@
             // 
             // pdf
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DarkRed;
-            this.ClientSize = new System.Drawing.Size(517, 609);
+            this.ClientSize = new System.Drawing.Size(915, 950);
             this.Controls.Add(this.btnGonder);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.axAcroPDF1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "pdf";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "HESAP BİLGİLERİ";
